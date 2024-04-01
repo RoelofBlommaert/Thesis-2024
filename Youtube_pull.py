8541c15ec4a51d229145ee8561255f79629b7107
RoelofBlommaert <roelof.blommaert@gmail.comgit config --global user.email roelof.blommaert@gmail.com>
2024-04-01 18:50:57 +0200

    Starting Youtube API testing

--- Youtube_pull.py
+++ Youtube_pull.py
@@ -1,9 +1,22 @@
 from googleapiclient.discovery import build
 import pandas as pd 
 import seaborn as sns
+import os
+api_key = os.getenv("MY_SECRET_KEY")
+tryout = 'vQ88JB-lgtQ'
 
-path = 'Superbowl video link document.xlsx'
-data = pd.read_excel(path, header=1, sheet_name=None)
+print(os.getenv("MY_SECRET_KEY"))
 
-print(data)
+youtube = build('youtube', 'v3', developerKey=api_key)
 
+
+# Define the file path to the Excel file
+file_path = 'Video_links.xlsx'
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(file_path)
+
+for i in df['Video_id']:
+    url_parts = i.split('?v=')[1]
+    API_ID=url_parts[0:11]
+    print(API_ID)
