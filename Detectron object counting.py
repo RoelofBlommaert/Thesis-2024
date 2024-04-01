a77dfb10c99b49ae9a5406fa3bc089338b633645
RoelofBlommaert <roelof.blommaert@gmail.comgit config --global user.email roelof.blommaert@gmail.com>
2024-04-01 16:18:04 +0200

    added DatasetCatalog to imports

--- Detectron object counting.py
+++ Detectron object counting.py
@@ -7,7 +7,7 @@ from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
+from detectron2.data import MetadataCatalog, DatasetCatalog
 
 # Setup detectron2 logger and configuration
 cfg = get_cfg()
