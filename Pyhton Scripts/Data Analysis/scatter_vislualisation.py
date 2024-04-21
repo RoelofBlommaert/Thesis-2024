0d93d6a22aef4bd62d95a24fb1de396d390d5e8a
RoelofBlommaert <roelof.blommaert@gmail.comgit config --global user.email roelof.blommaert@gmail.com>
2024-04-21 12:59:08 +0200

    Create scatter plot of measures

--- Pyhton Scripts/Data Analysis/scatter_vislualisation.py
+++ Pyhton Scripts/Data Analysis/scatter_vislualisation.py
@@ -10,20 +10,28 @@ visual = pd.read_csv(visual_path)
 youtube = pd.read_csv(youtube_path)
 merged = visual.merge(youtube, left_on='Video Name', right_on='id', how='outer')
 
-# Define the independent variables and dependent variables
-iv_columns = ['Color Complexity', 'Edge Density', 'Luminance Complexity']
-dv_columns = ['viewCount', 'likeCount', 'commentCount']  # Replace with actual DV column names
+# # Define the independent variables and dependent variables
+# iv_columns = ['Color Complexity', 'Edge Density', 'Luminance Complexity']
+# dv_columns = ['viewCount', 'likeCount', 'commentCount']  # Replace with actual DV column names
 
-# Create 3x3 subplots
-fig, axes = plt.subplots(3, 3, figsize=(15, 15))
+# # Create 3x3 subplots
+# fig, axes = plt.subplots(3, 3, figsize=(15, 15))
 
-# Loop through IVs and DVs to create scatter plots
-for i, dv in enumerate(dv_columns):
-    for j, iv in enumerate(iv_columns):
-        sns.scatterplot(data=merged, x=iv, y=dv, ax=axes[i, j])
-        axes[i, j].set_xlabel(iv)
-        axes[i, j].set_ylabel(dv)
+# # Loop through IVs and DVs to create scatter plots
+# for i, dv in enumerate(dv_columns):
+#     for j, iv in enumerate(iv_columns):
+#         sns.scatterplot(data=merged, x=iv, y=dv, ax=axes[i, j])
+#         axes[i, j].set_xlabel(iv)
+#         axes[i, j].set_ylabel(dv)
 
-# Adjust layout
-plt.tight_layout()
+# # Adjust layout
+# plt.tight_layout()
+# plt.show()
+
+FC_views = merged[['viewCount', 'likeCount', 'commentCount', 'Luminance Complexity',
+                    'Edge Density', 'Color Complexity']]
+DC_views = merged[['viewCount', 'likeCount', 'commentCount', 'Irregularity of Object Arrangement',
+                    'Asymmetry of Object Arrangement', 'Unique Objects Count']]
+g = sns.PairGrid(FC_views)
+g.map(plt.scatter)
 plt.show()
