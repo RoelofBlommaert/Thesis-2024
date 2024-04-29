f77d420ed23ce5e15ba1082c395888dda03b9338
RoelofBlommaert <roelof.blommaert@gmail.comgit config --global user.email roelof.blommaert@gmail.com>
2024-04-29 09:56:23 +0200

    Messing with NB params

--- Pyhton Scripts/Data Analysis/Regression.R
+++ Pyhton Scripts/Data Analysis/Regression.R
@@ -29,26 +29,36 @@ model_data <- data %>%
 # Print the data to check it
 print(head(model_data))
 
-
+na_count <- sapply(model_data, function(x) sum(is.na(x)))
+print("NA counts in each column:")
+print(na_count)
+# Check for NaN values across the dataframe
+nan_count <- sapply(model_data, function(x) sum(is.nan(x)))
+print("NaN counts in each column:")
+print(nan_count)
+# Check for Inf values across the dataframe
+inf_count <- sapply(model_data, function(x) sum(is.infinite(x)))
+print("Inf counts in each column:")
+print(inf_count)
 
 
 #Make formulas for regression
-lin_views_formula <- 'Views ~ Color.Complexity + Edge.Density + Luminance.Complexity + Asymmetry.of.Object.Arrangement +
+lin_views_formula <- 'Views ~ Color.Complexity + Edge.Density + Luminance.Complexity + 
 Irregularity.of.Object.Arrangement + Unique.Objects.Count + Visual.Variety + logSubscribers + Time + Length + status'
 
-quad_views_formula <- 'Views ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + squared_Asymmetry_of_OA +
+quad_views_formula <- 'Views ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + 
 squared_Irregularity_of_OA + squared_Unique_Objects_Count + squared_Visual_Variety + logSubscribers + Time + Length + status'
 
 lin_likes_formula <- 'Likes ~ Color.Complexity + Edge.Density + Luminance.Complexity + 
 Irregularity.of.Object.Arrangement + Unique.Objects.Count + Visual.Variety + logSubscribers + Time + Length + status'
 
-quad_likes_formula <- 'Likes ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + squared_Asymmetry_of_OA + 
+quad_likes_formula <- 'Likes ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + 
 squared_Irregularity_of_OA + squared_Unique_Objects_Count + squared_Visual_Variety + logSubscribers + Time + Length + status'
 
 lin_comment_formula <- 'Comments ~ Color.Complexity + Edge.Density + Luminance.Complexity +
 Irregularity.of.Object.Arrangement + Unique.Objects.Count + Visual.Variety + logSubscribers + Time + Length + status'
 
-quad_comment_formula <- 'Comments ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + squared_Asymmetry_of_OA + 
+quad_comment_formula <- 'Comments ~ squared_Color_Complexity + squared_Edge_Density + squared_Luminance_Complexity + 
 squared_Irregularity_of_OA + squared_Unique_Objects_Count + squared_Visual_Variety + logSubscribers + Time + Length + status'
 
 # Fit Poisson Model for Views as a baseline
@@ -58,18 +68,17 @@ summary(poisson_model_views)
 # Fit Negative Binomial Model for linear functions
 nb_model_views <- glm.nb(lin_views_formula, data = model_data, control = glm.control(maxit = 100))
 summary(nb_model_views)
-nb_model_likes <- glm.nb(lin_likes_formula, data = model_data, control = glm.control(maxit = 50))
+nb_model_likes <- glm.nb(lin_likes_formula, data = model_data, control = glm.control(maxit = 100))
 summary(nb_model_likes)
 nb_model_comments <- glm.nb(lin_comment_formula, data = model_data, control = glm.control(maxit = 100))
 summary(nb_model_comments)
 
 # Example for quadratic model (adjust accordingly for Likes and Comments)
-nb_model_views_quad <- glm.nb(quad_views_formula, data = model_data, control = glm.control(maxit = 100))
+control_settings <- glm.control(maxit = 100, epsilon = 1e-08, trace = TRUE)  # More conservative settings
+nb_model_views_quad <- glm.nb(quad_views_formula, data = model_data, control = control_settings)
 summary(nb_model_views_quad)
-nb_model_likes_quad <- glm.nb(quad_likes_formula, data = model_data, control = glm.control(maxit = 100))
+nb_model_likes_quad <- glm.nb(quad_likes_formula, data = model_data, control = control_settings)
 summary(nb_model_likes_quad)
-control_settings <- glm.control(maxit = 50, epsilon = 1e-08, trace = TRUE)  # More conservative settings
 nb_model_comments_quad <- glm.nb(quad_comment_formula, data = model_data, control = control_settings)
-nb_model_comments_quad <- glm.nb(quad_comment_formula, data = model_data, control = glm.control(maxit = 100))
 summary(nb_model_comments_quad)
 
